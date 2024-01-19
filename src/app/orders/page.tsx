cd94000409e9d9e4a9b2a36d3e71c31b20e8c5cd
Achmad96 <totsukamail3278@gmail.com>
2024-01-20 01:32:14 +0700

    modified files

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,10 +1,12 @@
 "use client";
-import Orders from "@/components/orders/Orders";
 import { AuthContext } from "@/context/AuthContextProvider";
 import { db } from "@/lib/firebase";
 import { UserRecord } from "firebase-admin/auth";
 import { getDocs, collection, query } from "firebase/firestore";
 import { useContext, useEffect, useState } from "react";
+import { OrderTypes } from "@/types";
+import { AnimatePresence, motion } from "framer-motion";
+import OrderItem from "@/components/orders/OrderItem";
 
 export default function OrdersPage() {
     const { currentUser }: { currentUser: UserRecord } = useContext(AuthContext);
@@ -16,12 +18,29 @@ export default function OrdersPage() {
                 const que = query(ordersCollection);
                 const datasDocs = await getDocs(que);
                 setOrders(datasDocs.docs.map(doc => doc.data()) as []);
-            } catch (error) {
-                console.error("Error fetching orders:", error);
+            } catch (error: any) {
+                console.error("Error fetching orders:", error.message);
             }
         }
         readDocs();
     }, []);
 
-    return <Orders orders={orders} user={currentUser} setOrders={setOrders} />;
+    return (
+        <div className="flex flex-col items-center py-5 min-h-[90dvh] gap-7 h-auto w-full">
+            <h1 className="text-4xl">Orders</h1>
+            <ul className="flex flex-col items-center justify-center gap-5 w-full">
+                <AnimatePresence>
+                    {orders?.length > 0 ? (
+                        orders.map((order: OrderTypes) => (
+                            <motion.li className="flex flex-col gap-5 items-center justify-center w-full" key={order.id} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+                                <OrderItem order={order} user={currentUser} setOrders={setOrders} />
+                            </motion.li>
+                        ))
+                    ) : (
+                        <p>Temporarily no orders</p>
+                    )}
+                </AnimatePresence>
+            </ul>
+        </div>
+    );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import SignInOutButton from "@/components/SignInOutButton";
 
 export default async function Navbar() {
     return (
-        <nav className="flex items-center justify-between w-full h-20 px-6 max-sm:h-16 max-sm:px-3">
+        <nav className="flex items-center justify-between w-full h-[10dvh] px-6 max-sm:h-16 max-sm:px-3">
             <div className="flex items-center">
                 <Image src={geotera} alt="Geotera" className="w-24 h-24" priority />
                 <p className="text-xl max-sm:hidden">Geotera</p>
