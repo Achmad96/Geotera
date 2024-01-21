fa6459276d5711150da661564756db662565f92c
Achmad96 <totsukamail3278@gmail.com>
2024-01-21 21:59:39 +0700

    modified files

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -9,7 +9,7 @@ export default function Alert({ title, message, visible, callback }: { title: st
     if (isVisible) {
         return (
             <div className="flex absolute left-0 right-0 top-0 bottom-0 w-full h-[90dvh] justify-center items-center">
-                <div className="flex absolute flex-col p-10 gap-10 bg-white shadow-2xl w-[30%] max-lg:w-[75%] rounded-xl">
+                <div className="flex absolute flex-col p-10 gap-10 bg-white shadow-2xl w-[30%] max-lg:w-[80%] rounded-xl">
                     <div className="flex flex-col gap-2">
                         <h1 className="text-2xl font-bold" id="confirmation-dialog">
                             {title}
@@ -17,10 +17,10 @@ export default function Alert({ title, message, visible, callback }: { title: st
                         <p id="confirmation-message">{message}</p>
                     </div>
                     <div className="flex gap-10 justify-center">
-                        <button className="bg-green-600 p-3 px-7 text-white shadow-2xl hover:bg-green-700 rounded-full" onClick={handleConfirm}>
+                        <button className="bg-green-600 p-3 px-7 max-sm:px-5 text-white shadow-2xl hover:bg-green-700 rounded-full" onClick={handleConfirm}>
                             Confirm
                         </button>
-                        <button className="bg-slate-50 p-3 px-7 shadow-2xl hover:bg-slate-100 rounded-full" onClick={handleClose}>
+                        <button className="bg-slate-50 p-3 px-7 max-sm:px-5 shadow-2xl hover:bg-slate-100 rounded-full" onClick={handleClose}>
                             Cancel
                         </button>
                     </div>
