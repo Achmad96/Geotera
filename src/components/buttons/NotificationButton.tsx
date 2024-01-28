eb900992072680f1efe676ca519a93f4a87341c0
Achmad96 <totsukamail3278@gmail.com>
2024-01-29 06:55:37 +0700

    modified file

--- src/components/buttons/NotificationButton.tsx
+++ src/components/buttons/NotificationButton.tsx
@@ -13,7 +13,7 @@ export default function NotificationButton() {
     return (
         <>
             <div className="daisy-indicator">
-                <span className="daisy-indicator-item daisy-badge daisy-badge-error mr-1 mt-1 daisy-badge-xs"></span>
+                {getNotifications && getNotifications.length > 0 && <span className="daisy-indicator-item daisy-badge daisy-badge-error mr-1 mt-1 daisy-badge-xs"></span>}
                 <IoNotifications className="w-6 h-6" onClick={() => {}} />
             </div>
         </>
