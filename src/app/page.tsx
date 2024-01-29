37cb20695b528fef205be7b0d46554f3580e0e60
Achmad96 <totsukamail3278@gmail.com>
2024-01-29 14:54:17 +0700

    modified files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,7 +49,7 @@ export default function HomePage() {
             </section>
             <section className="flex justify-center items-center w-full h-[90dvh] ">
                 <AnimatedText
-                    className="text-2xl tracking-wider md:text-center max-sm:text-justify w-1/2 max-sm:w-[80%]"
+                    className="text-2xl tracking-wider text-center w-1/2 max-sm:w-[80%]"
                     text={"Through our innovative technologies, we aim to empower consumers to make responsible choices that will reduce their carbon footprint and promote sustainable living."}
                     once={true}
                     underlineWords={[11, 12]}
--- src/components/buttons/NotificationButton.tsx
+++ src/components/buttons/NotificationButton.tsx
@@ -1,20 +1,50 @@
 "use client";
+import { NotificationsType } from "@/TypesNInterfaces";
 import { AuthContext } from "@/context/AuthContextProvider";
-import { useContext, useState } from "react";
+import { useContext, useReducer } from "react";
 import { IoNotifications } from "react-icons/io5";
 
-// const Notifications = () => {
-//     return <></>;
-// };
+type ActionType = {
+    type: "SET_NOTIFICATIONS" | "SET_VISIBILITY";
+    payload?: any;
+};
+
+type StateType = {
+    notifications: NotificationsType[];
+    visible: boolean;
+};
+
+const notificationsReducer = (state: StateType, action: ActionType) => {
+    const { type, payload } = action;
+    switch (type) {
+        case "SET_VISIBILITY":
+            return { ...state, visible: payload };
+        case "SET_NOTIFICATIONS":
+            return { ...state, notifications: payload };
+        default:
+            return { ...state };
+    }
+};
+
+const showNotifications = () => {
+    return (
+        <ul className="absolute right-0 top-7 daisy-menu bg-[#000] text-[#2FBC9B] w-56 rounded-box">
+            <li>Hello world</li>
+            <li>Halo dunia</li>
+            <li>Halo, apa kabar guys</li>
+        </ul>
+    );
+};
 
 export default function NotificationButton() {
     const { isAuth }: { isAuth: boolean } = useContext(AuthContext);
-    const [getNotifications, setNotifications] = useState([]);
+    const [state, dispatch] = useReducer(notificationsReducer, { notifications: [], visible: false });
     return (
         <>
             <div className="daisy-indicator">
-                {getNotifications && getNotifications.length > 0 && <span className="daisy-indicator-item daisy-badge daisy-badge-error mr-1 mt-1 daisy-badge-xs"></span>}
-                <IoNotifications className="w-6 h-6" onClick={() => {}} />
+                {state.notifications && state.notifications.length > 0 && <span className="daisy-indicator-item daisy-badge daisy-badge-error mr-1 mt-1 daisy-badge-xs"></span>}
+                <IoNotifications className="w-6 h-6" onClick={() => dispatch({ type: "SET_VISIBILITY", payload: !state.visible })} />
+                {state.visible && showNotifications()}
             </div>
         </>
     );
