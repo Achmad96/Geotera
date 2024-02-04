6f396b8a01f2570860cea853f5faa58d0b22cdd4
Achmad96 <totsukamail3278@gmail.com>
2024-02-05 01:41:29 +0700

    modified file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,7 +49,7 @@ export default function Home() {
                     />
                 </svg>
             </section>
-            <section id="about" className="flex justify-center items-center w-full max-w-full h-[90dvh]">
+            <section id="about" className="flex justify-center items-center w-full max-w-full h-dvh">
                 <AnimatedText
                     className="text-2xl -z-10 tracking-wider text-center w-1/2 max-sm:w-[80%]"
                     text={"Through our innovative technologies, we aim to empower consumers to make responsible choices that will reduce their carbon footprint and promote sustainable living."}
