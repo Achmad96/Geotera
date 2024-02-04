e7319458945c852ca358acff131e18ac85f799bf
Achmad96 <totsukamail3278@gmail.com>
2024-02-04 23:46:33 +0700

    modified file

--- src/components/buttons/HamburgerButton.tsx
+++ src/components/buttons/HamburgerButton.tsx
@@ -79,7 +79,7 @@ export default function HamburgerButton() {
     return (
         <div className="sm:collapse sm:hidden">
             <label className="daisy-btn daisy-btn-circle daisy-swap daisy-swap-rotate">
-                <input type="checkbox" checked={isMenuOpen} onClick={() => setIsMenuOpen(!isMenuOpen)} />
+                <input type="checkbox" checked={isMenuOpen} onChange={() => setIsMenuOpen(!isMenuOpen)} />
                 <svg className="daisy-swap-off daisy-fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512">
                     <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
                 </svg>
