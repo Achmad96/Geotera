d5a97544e29412ee009181c239747b2fd90cda99
Achmad96 <totsukamail3278@gmail.com>
2024-02-12 19:47:58 +0700

    refactor: remove console.log

--- src/utils/DateUtils.ts
+++ src/utils/DateUtils.ts
@@ -1,6 +1,5 @@
 export function isDateValid(date: Date) {
   const current_date = new Date();
-  console.log({ date, current_date });
   return (
     date >= current_date &&
     date.getHours() >= current_date.getHours() + 1 &&
