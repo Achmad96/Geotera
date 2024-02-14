624a06e4ed8e7a50425114bafab1ee94de6c2f31
Achmad96 <totsukamail3278@gmail.com>
2024-02-14 11:04:51 +0700

    style: format code

--- src/components/buttons/OrderButton.tsx
+++ src/components/buttons/OrderButton.tsx
@@ -1,33 +1,28 @@
 "use client";
 import { AuthContext, AuthContextType } from "@/providers/AuthProvider";
-import { defaultToastConfig } from "@/utils/ToastConfig";
+import { defaultToastConfig } from "@/utils/toastConfig";
 import { useContext } from "react";
 import { toast } from "react-toastify";
-import {
-  OrderModalContext,
-  OrderModalContextType,
-} from "@/providers/OrderModalProvider";
+import { OrderModalContext, OrderModalContextType } from "@/providers/OrderModalProvider";
 
 export default function OrderButton() {
-  const { isAuth } = useContext(AuthContext) as AuthContextType;
-  const { setIsModalOpen } = useContext(
-    OrderModalContext,
-  ) as OrderModalContextType;
-  return (
-    <button
-      className="btn-geo"
-      onClick={() => {
-        if (isAuth) {
-          setIsModalOpen(true);
-          return;
-        }
-        toast.error("You must be logged in", {
-          ...defaultToastConfig,
-          position: "top-right",
-        });
-      }}
-    >
-      Order now!
-    </button>
-  );
+    const { isAuth } = useContext(AuthContext) as AuthContextType;
+    const { setIsModalOpen } = useContext(OrderModalContext) as OrderModalContextType;
+    return (
+        <button
+            className="btn-geo"
+            onClick={() => {
+                if (isAuth) {
+                    setIsModalOpen(true);
+                    return;
+                }
+                toast.error("You must be logged in", {
+                    ...defaultToastConfig,
+                    position: "top-right",
+                });
+            }}
+        >
+            Order now!
+        </button>
+    );
 }
