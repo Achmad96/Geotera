c918344d4aa470436c408d01a907a6e1471df9c7
Achmad96 <totsukamail3278@gmail.com>
2024-02-14 11:02:32 +0700

    add time.test and modified time's util

---
+++ src/tests/time.test.ts
@@ -0,0 +1,8 @@
+import { isPickUpTimeValid } from "@/utils/time";
+
+describe("Date tests", () => {
+    it("should be equal or greater than 1 hour from now!", () => {
+        const date = new Date("February 13, 2024 15:28:00");
+        expect(isPickUpTimeValid(date)).toBe(true);
+    });
+});
---
+++ src/utils/time.ts
@@ -0,0 +1,4 @@
+export function isPickUpTimeValid(date: Date) {
+    const current_date = new Date();
+    return current_date.getTime() + 3600_000 <= date.getTime(); // +1 hour from current timestamp
+}
