8940a3c4325077dcd4c03ef806b790753a6ed34c
Achmad96 <totsukamail3278@gmail.com>
2024-02-17 13:39:09 +0700

    refactor: remove unnecessary code

--- next.config.js
+++ next.config.js
@@ -8,19 +8,6 @@ const nextConfig = {
             },
         ],
     },
-    async headers() {
-        return [
-            {
-                source: "/login",
-                headers: [
-                    {
-                        key: "Cross-Origin-Embedder-Policy",
-                        value: "unsafe-none",
-                    },
-                ],
-            },
-        ];
-    },
 };
 
 module.exports = nextConfig;
