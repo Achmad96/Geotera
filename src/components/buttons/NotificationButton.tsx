64da39816349fd92eebdf8815b008f62e61a353b
Achmad96 <totsukamail3278@gmail.com>
2024-02-17 20:15:24 +0700

    fix: notifications icon in mobile version

--- src/components/buttons/NotificationButton.tsx
+++ src/components/buttons/NotificationButton.tsx
@@ -1,12 +1,12 @@
 import { IoNotifications } from "react-icons/io5";
 
 export default function NotificationButton() {
-  return (
-    <>
-      <div className="daisy-indicator">
-        <span className="daisy-indicator-item daisy-badge daisy-badge-error mr-1 mt-1 daisy-badge-xs"></span>
-        <IoNotifications className="w-6 h-6" />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="daisy-indicator max-sm:collapse">
+                <span className="daisy-indicator-item daisy-badge daisy-badge-error mr-1 mt-1 daisy-badge-xs"></span>
+                <IoNotifications className="w-6 h-6" />
+            </div>
+        </>
+    );
 }
